81732fe538758fe74e13b3c2753d98ee6534024f
maxgunter99 <maxgunter99@gmail.com>
2019-11-05 16:14:59 -0600

    tests passing

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -21,25 +21,26 @@ def earliest_ancestor(ancestors, starting_node):
 
     print( 'Start from:' , starting_node )
 
+    # Loop through ancestors
     for i in ancestors:
 
+        # Add first ancestor to the starting node
         if i[1] == starting_node:
             parents.append( i[0] )
 
+        # If there are already added ancestors
         if len( parents ) >= 1:
             
+            # compare them
             if parents[ len( parents ) - 1 ] == i[1]:
                 parents.append( i[0] )
 
 
         elif i[0] == starting_node:
-
             print( 'start - ' , i )
-
             parents.append( i[0] )
 
-    print( 'Parents:' , parents )
-
+    # In case I missed any
     for i in parents:
 
         for x in ancestors:
@@ -63,14 +64,44 @@ def earliest_ancestor(ancestors, starting_node):
             noDupes.append( i )
             index += 1
 
+    split = []
+
+    # check for the same child and return smaller of the two parents
+    for i in ancestors:
+
+            # If there are two ancestors
+            if len( parents ) == 2:
+
+                # Add [ parent , child ] to split
+                if parents[0] == i[0]:
+                    print( f' {parents[0]}s Child is {i[1]}' )
+                    split.append( [ parents[0] , i[1] ] )
+
+                # Add [ parent , child ] to split
+                elif parents[1] == i[0]:
+                    print( f' {parents[1]}s Child is {i[1]}' )
+                    split.append( [ parents[1] , i[1] ] )
+
+    # Look through split and compare if the 
+    for i in range( len( split ) - 1 ):
+
+        # If the current parent has the same child as the next parent, return the smaller number
+        if split[i][1] == split[ i + 1 ][1]:
+
+            if split[i][0] >= split[ i + 1 ][0]:
+                return split[ i + 1 ][0]
+
+            else:
+                return split[ i ][0]
+
 
     print( 'Parents:' , parents )
+    print( 'SPLIT' , split )
 
-    # if len( parents ) == 2:
-    #     return parents[0]
-
+    # if there are more than 2 ancestors from different generations, return the last ( oldest )
     if len( parents ) >= 2:
         return parents[ len( parents ) - 1 ]
 
+    # If no ancestors, return -1
     else:
         return -1
